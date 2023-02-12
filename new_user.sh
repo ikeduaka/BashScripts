7fcb9e94f0f986aebca2ca9d1cae9c368b4bb66d
root <root@ip-172-31-0-215.ec2.internal>
2023-02-12 03:40:54 +0000

    new file to add new aws user

---
+++ new_user.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# create-user
+
+[--path <value>]
+--user-name <Value>
+[--permission-boundary <value>]
+[--tags <value>]
+[--cli-input-json | --cli-input-yaml]
+[--generate-cli-skeleton <value>]
+
+# create-login-profile
+
+--user-name <value>
+--password <value>
+[--password-reset-required | --no-password-reset-required]
+[--cli-input-json | --cli-input-yaml]
+[--generate-cli-skeleton <value>]
+
